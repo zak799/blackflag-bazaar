6a54dcad9235cf8432650ffb73c19db48b13939b
Zakariya <zaksteryt@gmail.com>
2026-01-06 21:58:46 +0000

    Update Directory final i hope ffs

--- api/public/index.php
+++ api/public/index.php
@@ -1,19 +1,16 @@
 <?php
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$path = trim($path, '/');
+$path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
 $routes = [
-    '' => 'app/index.php',
-    'info' => 'app/info.php',
-    'renew' => 'app/renew.php',
-    'upgrade' => 'app/upgrade.php',
+    '' => 'public/index.php',
+    'info' => 'public/info.php',
+    'renew' => 'public/renew.php',
+    'upgrade' => 'public/upgrade.php',
 ];
 
 if (isset($routes[$path])) {
     require __DIR__ . '/' . $routes[$path];
 } else {
     http_response_code(404);
-    // redirect to 404.php inside app
-    require __DIR__ . '/app/404.php';
-    exit;
+    require __DIR__ . '/404.php';
 }
