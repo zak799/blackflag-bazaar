737425fc69d47078f6678dfeca74fb8006dea1f1
Zakariya <zaksteryt@gmail.com>
2026-01-06 21:33:15 +0000

    Update Directories x2

--- api/index.php
+++ api/index.php
@@ -1,1 +1,22 @@
-Hello
+<?php
+declare(strict_types=1);
+header('Content-Type: text/html; charset=UTF-8');
+
+// Front controller: catch everything
+$request = $_SERVER['REQUEST_URI'];
+$request = strtok($request, '?'); // remove query string
+$request = rtrim($request, '/');
+
+// Map routes to your public PHP files
+$routes = [
+    '' => __DIR__ . '/../public/index.php',
+    '/index' => __DIR__ . '/../public/index.php',
+    '/info' => __DIR__ . '/../public/info.php',
+    '/renew' => __DIR__ . '/../public/renew.php',
+    '/upgrade' => __DIR__ . '/../public/upgrade.php'
+];
+
+// Default fallback: index.php
+$target = $routes[$request] ?? __DIR__ . '/../public/index.php';
+
+include $target;
